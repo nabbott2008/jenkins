0f6f3d3bc9c37aabd1fb8a6ca894aace18bf2d4c
Nicholas Abbott <nabbott2008@gmail.com>
2019-10-31 10:00:28 +0000

    test

---
+++ test.groovy
@@ -0,0 +1,23 @@
+def testInput
+stage('first') {
+    try {
+        timeout(1440) {
+	    testInput = input message: "Provide input", parameters: [choice(choices: existingProductVersions, description: "select", name: 'lastInput')]
+            currentBuild.displayName = "${testInput}"
+
+        }
+    } catch(err) {
+        echo "No input"
+        return
+    }
+    node() {
+        try {
+	    deleteDir()
+            retry(3) {
+                sh """#!/bin/bash -x
+		    ls -l
+                """
+            }
+	}
+    }
+}
